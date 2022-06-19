df26d72b0f3c5efe35ae99aba2adbce25216bf84
francribaj <francribaj@gmail.com>
2022-06-19 06:34:34 +0200

    chapter_2_solved

---
+++ 2.15_Exercises.py
@@ -0,0 +1,26 @@
+# '''
+# Exercise 2: Write a program that uses input to prompt a user for their
+# name and then welcomes them.
+# '''
+# name = input("Type your name:")
+# print("Hello" + name )
+
+# '''
+# Exercise 3: Write a program to prompt the user for hours and rate per
+# hour to compute gross pay.
+# '''
+
+# hours = input("Enter hours:")
+# rate = input("ENTER rate:")
+
+# gross = int(hours) * float(rate) 
+# print("Pay:" + str(gross))
+
+'''
+Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the
+converted temperature.
+'''
+
+temp_celsius = input("Temperature in Celsius:")
+temp_fahrenheit = (float(temp_celsius) * 9/5) + 32 
+print("Temperature in Fahrenheit:" + str(temp_fahrenheit))
