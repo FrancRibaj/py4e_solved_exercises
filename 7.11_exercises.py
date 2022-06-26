3446bb3f75d7cfeb06dc7d5e3b11309da7c8da7a
francribaj <francribaj@gmail.com>
2022-06-26 23:57:09 +0200

    8.16_exercises not finished

--- 7.11_exercises.py
+++ 7.11_exercises.py
@@ -47,5 +47,46 @@ for line in fhand:
 average = total / count
 print("Average spam confidence:", average)
 
+"""
+Exercise  7.3: Sometimes when programmers get bored or want to have a bit of
+fun, they adda harmless Easter Egg to their program. Modify the program that
+prompts the user for a file name so that is prints a funny message when the
+the user types in the exact file name "na na boo boo". the program should
+behave normally for all other files which exist and don't exit. Here is a
+sample execution of the program:
+
+python egg.py
+Enter the file name: mbox.txt
+There were 1797 subject lines in mbox.txt
+
+python egg.py
+Enter the file name: missing.tyxt
+File cannot be opened: missing.tyxt
+
+python egg.py
+Enter the file name: na na boo boo
+NA NA BOO BOO TO YOU - You have been punk'd!
+
+Python for Everybody: Exploring Data Using Python 3
+by Charles R. Severance
+
+Solution by Jamison Lahman, May 31, 2017
+"""
+
+prompt = input("Enter the file name:")
+try:
+    if prompt == 'na na boo boo':
+        print("NA NA BOO BOO TO YOU - You have been punk'd!")
+        exit()
+    fhand = open(prompt)
+except FileNotFoundError:
+    print("File cannot be opened:", prompt)
+    exit()
+
+
+
+
+
+
 
 
